785b70f11d21e231065e613143eb85b49fd61738
pmadinga <phiwen2@gmail.com>
2022-02-28 21:54:32 +0200

    Attempt to fetch and made dummy renderings

--- src/app.js
+++ src/app.js
@@ -1,2 +1,25 @@
 // Declare url
-const APP_URL = 'https://restcountries.com/v3.1/all';
+const APP_URL = `https://restcountries.com/v3.1/all`;
+
+const fetchCountries = (APP_URL) => {
+    try {
+        fetch(APP_URL)
+        .then(res => res.json())
+        .then(data => countryInfo(data))
+        .catch(e => console.log(e.message))
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+const countryInfo = data => {
+    // loop throu countries
+    for(let i = 0; i <= 10; i++){
+        const countryName = document.createElement('h2');
+        countryName.innerHTML = data[i].name.official
+
+        document.body.appendChild(countryName)
+    }
+} 
+
+window.onload = fetchCountries(APP_URL);
